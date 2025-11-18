83b50537da4a91aee175792edddf72fc41d5933b
Akshaya Kumar Mahanty <akshayakumarmahanty@LAP831PER-Akshayas-MacBook.local>
2025-11-18 22:47:31 +0530

    create new /user api

---
+++ src/main/java/com/ak/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.ak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/user")
+    public String hello() {
+        return "Hello User";
+    }
+}
